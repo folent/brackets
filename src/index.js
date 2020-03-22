25cad4e3212f7e12a0479b2f3a4f65a664eb9788
folent <folent1@yandex.ru>
2020-03-22 23:14:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    for (let i = 0; i < bracketsConfig.length;) {
+
+        let arr = bracketsConfig[i].join("");
+
+        if (!str.includes(arr)) {
+            i++;
+            continue;
+        } else {
+            str = str.replace(arr, "");
+            i = 0;
+        }
+    }
+    return str.length == 0 ? true : false;
+
 }
